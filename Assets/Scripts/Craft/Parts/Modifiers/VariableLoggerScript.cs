cd492b5aa27f9de5a00d7786f89736c66740e44f
Paul Wheeler <paul@invioinc.com>
2020-05-05 00:36:10 -1000

    Introducing SR2LoggerPlus
     * There is now a part property setting for sampling frequency instead
     of using a special variable.
     * There is now a part property setting to enable logging to a file.
     * There is now a part property setting to include all log messages.
     * Variable sampling now works in time warp mode (with a scaled
     frequency).
     * A timestamp is now included with each sample.
     * The receiver scrypt is now python 2.7 and 3 compatible, and outputs
     valid yaml documents.
    
    Note: The default UDP port has changed to 2837 to avoid confusion.

--- Assets/Scripts/Craft/Parts/Modifiers/VariableLoggerScript.cs
+++ Assets/Scripts/Craft/Parts/Modifiers/VariableLoggerScript.cs
@@ -1,125 +1,231 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using ModApi.Craft.Parts;
+using ModApi.Craft.Program;
+using ModApi.GameLoop;
+using ModApi.GameLoop.Interfaces;
+using UnityEngine;
+
 namespace Assets.Scripts.Craft.Parts.Modifiers
 {
-    using Assets.Scripts.Logger;
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Reflection;
-    using System.Text;
-    using ModApi.Craft.Parts;
-    using ModApi.Craft.Program;
-    using ModApi.GameLoop.Interfaces;
-    using UnityEngine;
-    using ModApi.GameLoop;
-
-    public class VariableLoggerScript : PartModifierScript<VariableLoggerData>, IFlightFixedUpdate
+    public class VariableLoggerScript : PartModifierScript<VariableLoggerData>, IFlightUpdate
     {
-        #region Static Members (reflection)
-        static FieldInfo _processField;
+        private const String Version = "0.1-beta";
+
+        private static readonly FieldInfo _processField;
+        private static readonly PropertyInfo _logServiceProperty;
 
         static VariableLoggerScript()
         {
             _processField = typeof(FlightProgramScript).GetField("_process", BindingFlags.NonPublic | BindingFlags.Instance);
+            _logServiceProperty = typeof(Process).GetProperty("LogService", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
         }
-        #endregion Static Members (reflection)
 
+        private Boolean initialized;
 
+        private FlightProgramScript flightProgramScript;
 
-        bool _inited = false;
+        private UdpServer server;
+        private StreamWriter logFileWriter;
 
-        FlightProgramScript flightProgramScript;
-
-        LoggerServer server;
-
-        float sampleRate = 0;
-
-        float samplePeriod = 0;
-
-        float timeSinceLast = 0;
+        private Single frequency;
+        private Single timeSinceLastSample;
+        private TimeSpan worldTime = TimeSpan.Zero;
 
         /// <summary>
         /// Initialises this instance.
         /// </summary>
-        private void EnsureInitialised()
+        private Boolean EnsureInitialized()
         {
-            if (!_inited)
+            if (!this.initialized)
             {
+                Debug.Log($"Initializing {nameof(VariableLoggerScript)} {Version}");
+
                 // get flight program
-                flightProgramScript = PartScript.GetModifier<FlightProgramScript>();
-                if (flightProgramScript == null)
+                this.flightProgramScript = PartScript.GetModifier<FlightProgramScript>();
+                if (this.flightProgramScript == null)
                 {
                     Debug.LogError("Logger script has no flight program: deactivating");
-                    enabled = false;
-                    return;
-                }
-
-                server = new LoggerServer(Data.Hostname, Data.Port);
-
-                _inited = true;
-            }
-        }
-
-        public void FlightFixedUpdate(in FlightFrameData frame)
-        {
-            if (frame.IsPaused || frame.IsWarping || !PartScript.Data.Activated) { return; }
-            EnsureInitialised();
-
-            var rate = (float?)flightProgramScript.GetGlobalVariable("LogFrequency")?.NumberValue ?? 0f;
-            if (!Mathf.Approximately(rate, sampleRate))
-            {
-                sampleRate = rate;
-                samplePeriod = 1 / rate;
-                timeSinceLast = 0f;
-            }
-
-            timeSinceLast += (float)frame.DeltaTimeWorld;
-            while (timeSinceLast > samplePeriod)
-            {
-                timeSinceLast -= samplePeriod;
-                SendUpdate();
-            }
-        }
-
-        private void SendUpdate()
-        {
-            server.ResetState();
-            var w = server.Writer;
-            Process process = (Process)_processField.GetValue(flightProgramScript);
-            foreach (var v in process.GlobalVariables.Variables)
-            {
-                if (v.Name.StartsWith("log_") && v.Name.Length > 4)
+                    this.enabled = false;
+                } else
                 {
-                    server.SendName(v.Name.Substring(4, v.Name.Length - 4));
-                    var val = v.Value;
-                    switch (val.ExpressionType)
+                    var process = (Process)_processField.GetValue(flightProgramScript);
+
+                    // Inject tracing log service.
+                    var tracer = new TracingLogService(this, process.LogService);
+                    // Set the default ILogService for new threads
+                    _logServiceProperty.SetValue(process, tracer);
+                    // Update all existing threads
+                    foreach (var thread in process.Threads)
                     {
-                        case ExpressionType.Boolean:
-                            w.Write((byte)TypeCodes.Boolean);
-                            w.Write(val.BoolValue);
-                            break;
-                        case ExpressionType.Number:
-                            w.Write((byte)TypeCodes.Float64);
-                            w.Write(val.NumberValue);
-                            break;
-                        case ExpressionType.Vector:
-                            w.Write((byte)TypeCodes.Vector3d);
-                            w.Write(val.VectorValue.x);
-                            w.Write(val.VectorValue.y);
-                            w.Write(val.VectorValue.z);
-                            break;
-                        default:
-                            Debug.LogWarning("Cannot log type: " + val.ExpressionType);
-                            w.Write((byte)TypeCodes.Null);
-                            break;
+                        thread.Context.Log = tracer;
+                    }
+
+                    if (!String.IsNullOrEmpty(this.Data.Hostname))
+                    {
+                        Debug.Log($"Logging Data To udp://{this.Data.Hostname}:{this.Data.Port}");
+                        this.server = new UdpServer(this.Data.Hostname, this.Data.Port);
+                    }
+
+                    if (!String.IsNullOrEmpty(this.Data.Path))
+                    {
+                        var absPath = Path.Combine(Application.persistentDataPath, this.Data.Path);
+                        Debug.Log($"Logging Data To '{absPath}'");
+                        this.logFileWriter = new StreamWriter(absPath, append: true);
+                    }
+
+                    // Convert from frequency in milliseconds to frequency in seconds
+                    this.frequency = this.Data.Frequency / 1000f;
+
+                    this.initialized = true;
+                    Debug.Log($"{nameof(VariableLoggerScript)} successfully initialized.");
+                }
+            }
+
+            return this.initialized;
+        }
+
+        public void FlightUpdate(in FlightFrameData frame)
+        {
+            if (frame.IsPaused || !PartScript.Data.Activated || !this.EnsureInitialized())
+            {
+                return;
+            }
+
+            this.worldTime = TimeSpan.FromSeconds(frame.FlightScene.FlightState.Time);
+            this.timeSinceLastSample += (float)frame.DeltaTimeWorld;
+            var factor = (float)(frame.TimeManager.CurrentMode.TimeMultiplier > 2.0 ? (frame.TimeManager.CurrentMode.TimeMultiplier / 2.0) : 1.0);
+            while (this.timeSinceLastSample > this.frequency * factor)
+            {
+                this.timeSinceLastSample -= this.frequency * factor;
+                SampleVariables();
+            }
+        }
+
+        private void LogMessage(String message, Int32 activeThreadId)
+        {
+            if (this.Data.IncludeLog)
+            {
+                var formattedMessage =
+                    this.worldTime.TotalDays >= 1 ?
+                        $"[{this.worldTime:d' days 'hh\\:mm\\:ss\\.fff}] <{activeThreadId}>: {message}" :
+                        $"[{this.worldTime:hh\\:mm\\:ss\\.fff}] <{activeThreadId}>: {message}";
+
+                this.server?.SendLogMessage(formattedMessage);
+                if (this.logFileWriter != null)
+                {
+                    lock (this.logFileWriter)
+                    {
+                        this.logFileWriter.WriteLine(formattedMessage);
+                        this.logFileWriter.Flush();
                     }
                 }
             }
-            server.FinishSend();
+        }
+
+        private void SampleVariables()
+        {
+            var stringBuilder = this.logFileWriter != null ? new StringBuilder() : null;
+            this.server?.BeginVariableSample(this.worldTime);
+            try
+            {
+
+                var first = true;
+                var process = (Process)_processField.GetValue(flightProgramScript);
+                foreach (var v in process.GlobalVariables.Variables)
+                {
+                    if (v.Name.StartsWith("log_") && v.Name.Length > 4)
+                    {
+                        var friendlyName = v.Name.Substring(4, v.Name.Length - 4);
+                        this.server?.SendVariable(friendlyName, v.Value);
+                        if (!first)
+                        {
+                            stringBuilder?.Append(", ");
+                        } else
+                        {
+                            first = false;
+                        }
+
+                        stringBuilder?.Append($"'{friendlyName}': {ConvertToJson(v.Value)}");
+                    }
+                }
+            }
+            finally
+            {
+                this.server?.FinishVariableSample();
+            }
+
+            if (this.logFileWriter != null)
+            {
+                lock (this.logFileWriter)
+                {
+                    this.logFileWriter.WriteLine($"{{ 'timestamp': '{this.worldTime}', 'variables': {{ {stringBuilder} }} }}");
+                    this.logFileWriter.Flush();
+                }
+            }
+        }
+
+        private static String ConvertToJson(ExpressionResult value)
+        {
+            switch (value.ExpressionType)
+            {
+                case ExpressionType.Boolean:
+                    return value.BoolValue ? "true" : "false";
+                case ExpressionType.Number:
+                    return value.NumberValue.ToString("R");
+                case ExpressionType.Text:
+                    return $"'{Quote(value.TextValue)}'";
+                case ExpressionType.Vector:
+                    return $"{{ 'x': {value.VectorValue.x}, 'y': {value.VectorValue.y}, 'z': {value.VectorValue.z} }}";
+                case ExpressionType.List:
+                    return $"[{String.Join(", ", value.ListValue.Select(Quote))}]";
+                default:
+                    Debug.LogWarning("Cannot log type: " + value.ExpressionType);
+                    return "null";
+            }
+        }
+
+        private static String Quote(String value)
+        {
+            return value.Replace("\\", "\\\\").Replace("'", "\\'");
         }
 
         public override void FlightEnd()
         {
-            server.Dispose();
+            this.logFileWriter?.Dispose();
+            this.server?.Dispose();
+        }
+
+        private class TracingLogService : ILogService
+        {
+            private readonly VariableLoggerScript parent;
+            private readonly ILogService innerLogService;
+
+            public TracingLogService(VariableLoggerScript parent, ILogService innerLogService)
+            {
+                this.parent = parent;
+                this.innerLogService = innerLogService;
+            }
+
+            public void Log(string message, IThreadContext context = null, ProgramNode node = null)
+            {
+                this.parent.LogMessage(message, this.innerLogService.ActiveThreadId ?? -1);
+                this.innerLogService.Log(message, context, node);
+            }
+
+            public void LogError(string message, IThreadContext context = null, ProgramNode node = null)
+            {
+                this.innerLogService.LogError(message, context, node);
+            }
+
+            public int? ActiveThreadId
+            {
+                get => this.innerLogService.ActiveThreadId;
+                set => this.innerLogService.ActiveThreadId = value;
+            }
         }
     }
 }
--- recevier.py
+++ recevier.py
@@ -1,7 +1,10 @@
-import socket, struct
+#! /usr/bin/python
+
+import socket, struct, sys
+from datetime import timedelta
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(("localhost", 2873))
+s.bind(("localhost", 2837))
 
 TypeFormats = [
     "",     # null
@@ -9,41 +12,67 @@ TypeFormats = [
     "?",    # bool
     "ddd"]  # Vector3d
 
+
 class Packet:
     def __init__(self, data):
         self.data = data
         self.pos = 0
-    def get(self, l): # get l bytes
+
+    def get(self, l):  # get l bytes
         self.pos += l
         return self.data[self.pos - l:self.pos]
-    def read(self, fmt): #  read all formatted values
-        v = self.readmult(fmt)
+
+    def read(self, fmt):  # read all formatted values
+        v = self.readAll(fmt)
         if len(v) == 0: return None
         if len(v) == 1: return v[0]
         return v
-    def readmult(self, fmt): # read multiple formatted values
+
+    def readAll(self, fmt):  # read multiple formatted values
         return struct.unpack(fmt, self.get(struct.calcsize(fmt)))
+
     @property
-    def more(self): # is there more data?
+    def more(self):  # is there more data?
         return self.pos < len(self.data)
+
 
 def readPacket(dat):
     p = Packet(dat)
 
-    while p.more:
-        nameLen = p.read("H")
-        name = p.get(nameLen).decode()
-        
-        tp = p.read("B")
-        typeCode = TypeFormats[tp]
-        val = p.read(typeCode)
+    messageType = p.read("B")
 
-        print(name, "=", val)
+    print("---")
+    if messageType == 1:
+        print("sample:")
+        timestamp = p.read("Q")
+        print("  timestamp: {0}".format(timedelta(milliseconds=timestamp)))
+        print("  variables:")
+        while p.more:
+            nameLen = p.read("I")
+            name = p.get(nameLen).decode('utf-8')
 
-    print("")
+            tp = p.read("B")
+            if tp == 4:
+                # Text
+                textLen = p.read("I")
+                val = "'{0}'".format(p.get(textLen).decode('utf-8').replace("\\", "\\\\").replace("'", "\\'"))
+            else:
+                structFormat = TypeFormats[tp]
+                val = p.read(structFormat)
 
-print("Starting")
+            print("    {0}: {1}".format(name, val))
+    elif messageType == 2:
+        messageLen = p.read("I")
+        message = p.get(messageLen).decode('utf-8')
+        print("log: >-")
+        print("  {0}".format(message))
+    else:
+        raise Exception("Unexpected message type: " + str(messageType))
+
+    sys.stdout.flush()
+
+
+sys.stderr.write("Starting Client...\n")
 while 1:
     d, a = s.recvfrom(2048)
-
     readPacket(d)
